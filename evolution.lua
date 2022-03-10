00cc43b8650360570796f7636f0fded55711afa0
Zardar <doomiurg@gmail.com>
2022-03-11 00:50:22 +0500

    исполнен юзер инпут что-то намудрил с возвратами, поправлю завтра

--- evolution.lua
+++ evolution.lua
@@ -10,24 +10,77 @@
 --Проинициируем массивы. они будут носить заданный размер 248*248
 --Этот размер связан исключительно с удобством вывода на экран
 --почти максимального поля и, возмоно, инфопанели
-local unicode=require('unicode')
-local gpu=require('component').gpu
-local os=require('os')
+local computer=require('computer')
+local unicode = require('unicode')
+local gpu = require('component').gpu
+local term = require('term')
+local os = require('os')
 local text = ''
 local x_dim, y_dim = gpu.getResolution()
-xs=x_dim*2
-ys=y_dim*4-8
-local iter=0
-local dots=5
+local iter = 0
+local dots = 0
+local changes = 0
+local neighbors = 0
 local field, screen, actualScreenChanges,chars = {},{},{},{}
+local l,r = 'left','right'
+local pullSignal=computer.pullSignal
+local events = {}
+events.new ={touch='touch',drag='drag',drop='drop'}
+local user_draw={unicode.char(0x2800),unicode.char(0x28ff)}
+local mode = ''
+screen.left,screen.right = {},{}
+xs = x_dim*2
+ys = y_dim*4-8
+
+--перехват ивентов. надстройка над ОС
+function computer.pullSignal(...)
+    local e={pullSignal(...)}
+    for event in pairs(events.new)do 
+        if event==e[1] then
+            return events[event](e)
+        end
+    end
+    return table.unpack(e) 
+end
+
+function userSet(y,x,c)
+    local txt=user_draw[c]..user_draw[c]
+    gpu.set(x*2,y,txt)
+    return true
+end
+
+--1touch 2addres 3x 4y 5 0or1 = LorR
+function events.touch(e)
+    local x=(e[3]-e[3]%2)/2
+    local y=e[4]
+    if mode == 'play' then 
+        if x < 45 and x>39 and y > y_dim-1 then
+            return userInput()
+        else
+            return 'showMustGoOne'
+        end
+    end
+    if x < 6 and y > y_dim-1 then 
+        return goToPlay()
+    end
+    field[y][x]=e[5]
+    local c=e[5]+1
+    return userSet(y,x,c)
+end
+function events.drag(e)
+    return events.touch(e)
+end
+function events.drop(e)
+    return events.touch(e)
+end
+
+
 --field содержит опорную информацию о поле для вычислений
 --screen содержит перечень узлов field, к которым необходимо
 --прявить пристальное внимание и выяснить их дальнейшую судьбу
 --actualScreenChanges представляет собой список узлов
 --сменивших своё состояние в течении текущей итерации.
 --проинициируем все узлы таблиц в field и таблицы в screen
-screen.left,screen.right = {},{}
-local l,r = 'left','right'
 local function tablesInit()
 	for y = 1,ys do 
 	    field[y] = {}
@@ -37,46 +90,45 @@ local function tablesInit()
 	        field[y][x] = 0
 	    end
 	end
-	local ch_y=(ys-ys%4)/4
-	local ch_x=(xs-xs%4)/2
-	for y=1,ch_y do 
+	local ch_y = (ys-ys%4)/4
+	local ch_x = (xs-xs%4)/2
+	for y = 1,ch_y do 
         chars[y]={}
-	    for x=1,ch_x do 
-            chars[y][x]=0x2800
+	    for x = 1,ch_x do 
+            chars[y][x] = 0x2800
 	    end
     end    
-	return 'tadaaa'
-    
+	return userInput()   
 end
 --опишем биткарту шрифта брайля
 local bits = {1,8,2,16,4,32,64,128}
 --попробуем описать трансформацию значений массива в шрифт брайля
 function toUnicode()
 	for y in pairs(actualScreenChanges) do
-		local ch_y=math.floor((y-y%4)/4)+1--1,
+		local ch_y = math.floor((y-y%4)/4)+1--1,
 		for x in pairs(actualScreenChanges[y]) do
-			local ch_x=math.floor((x-x%2)/2)+1
+			local ch_x = math.floor((x-x%2)/2)+1
 			--единица равна сету, используется or
 			--ноль равен войду, используется and
-			chars[ch_y][ch_x]=chars[ch_y][ch_x]+bits[(y%4)*2+x%2+1]*actualScreenChanges[y][x]
+			chars[ch_y][ch_x] = chars[ch_y][ch_x]+bits[(y%4)*2+x%2+1]*actualScreenChanges[y][x]
 		end
 	end
-	return true
+	return showMustGoOne()
 end
 
 --попросим пользователя внести начальные данные
-function userInput(l)
-	field[23][23]=1 field[23][24]=1 field[22][25]=1 field[24][25]=1 field[24][24]=1
-
-    return 'получили данные'
+function userInput()
+    mode = 'edit'
+    gpu.set(1,y_dim,'play')
+    return true
 end
 
 --функция вычисления координат прилегающих клеток
-local function getAdjoining(n,ns)
+function getAdjoining(n,ns)
 local yl, yr = 1,1
     if n > 1 and n < ns then 
-        yl=n-1 
-        yr=n+1 
+        yl = n-1 
+        yr = n+1 
     else
         if n == 1 then 
             yl, yr = ns, n+1
@@ -88,26 +140,27 @@ local yl, yr = 1,1
 end
 
 --здесь создаются целеуказания для проверок напряжений узлов
-local function setScreen(yl,y,yr,xl,x,xr,s)
-    screen[s][yl][x]='?'
-    screen[s][y][x]='?'
-    screen[s][yr][x]='?'
-    screen[s][yl][xl]='?'
-    screen[s][y][xl]='?'
-    screen[s][yr][xl]='?'
-    screen[s][yl][xr]='?'
-    screen[s][y][xr]='?'
-    screen[s][yr][xr]='?'
+function setScreen(yl,y,yr,xl,x,xr,s)
+    screen[s][yl][x] = '?'
+    screen[s][y][x] = '?'
+    screen[s][yr][x] = '?'
+    screen[s][yl][xl] = '?'
+    screen[s][y][xl] = '?'
+    screen[s][yr][xl] = '?'
+    screen[s][yl][xr] = '?'
+    screen[s][y][xr] = '?'
+    screen[s][yr][xr] = '?'
 return 'completed'
 end
 
 --сохраняем первичный список состояний узлов
-local function saveChanges()--получаем имя таблицы
+function saveChanges()--получаем имя таблицы
     for y in pairs(field)do
        local yl,yr = getAdjoining(y,ys)
         for x in pairs(field[y])do 
             if field[y][x] == 1 then 
-                actualScreenChanges[y][x]=1
+                actualScreenChanges[y][x] = 1
+                dots = dots + 1
                 local xl,xr = getAdjoining(x,xs)
                 setScreen(yl,y,yr,xl,x,xr,l)
             end 
@@ -115,9 +168,14 @@ local function saveChanges()--получаем имя таблицы
     end
 --вопросом у нас помечены все узлы, где
 --в следующий цикл будут произведены проверки
-return 'addition complite'
+return toUnicode()
 end
-
+function goToPlay()
+    mode = 'play'
+    term.clear()
+    gpu.set(40,y_dim,'STOP')
+    return saveChanges()
+end
 --теперь попробуем произвести вычисления
 --определяющие состояние поля в следующей итерации.
 --для этого нам понадобится пройти по таблице screen,
@@ -129,10 +187,10 @@ end
 --3.таблица-указатель, на основе которой произойдут рассчёты.
 --4.пустая таблица-указатель
 function whatNews(l,r)--left and right sides
-    local neighbors = 0
+    changes = 0
     for y=1,ys do 
-    	screen[r][y]={}
-		actualScreenChanges[y]={}
+    	screen[r][y] = {}
+		actualScreenChanges[y] = {}
     end
     --получаем из левого экрана сведения о узлах 
     --реалии которых нам интересны 
@@ -145,19 +203,21 @@ function whatNews(l,r)--left and right sides
             field[yl][xl] + field[yl][x] + field[yl][xr] +
             field[yr][xl] + field[yr][x] + field[yr][xr]
             if neighbors == 3 then 
-                if field[y][x]==0 then
+                if field[y][x] == 0 then
                     setScreen(yl,y,yr,xl,x,xr,r)
                 	--узел ожил
-                    dots=dots+1
-                	actualScreenChanges[y][x]=1
+                    dots = dots+1
+                    changes=changes+1
+                	actualScreenChanges[y][x] = 1
                 end
             else 
                 if neighbors ~= 2 then 
-                    if field[y][x]==1 then
+                    if field[y][x] == 1 then
                         setScreen(yl,y,yr,xl,x,xr,r)
                     	--узел погиб
-                        dots=dots-1
-                    	actualScreenChanges[y][x]=-1
+                        dots = dots-1
+                        changes = changes+1
+                    	actualScreenChanges[y][x] = -1
                     end
                 end 
             end 
@@ -170,10 +230,10 @@ function whatNews(l,r)--left and right sides
     --произведём имплементацию изменений
     for y in pairs(actualScreenChanges) do
         for x in pairs(actualScreenChanges[y]) do
-            field[y][x]=field[y][x]+actualScreenChanges[y][x]
+            field[y][x] = field[y][x] + actualScreenChanges[y][x]
         end
     end
-    return 'calculations completed'
+    return toUnicode()
 end
 
 --выведем изменения на монитор
@@ -183,46 +243,44 @@ function showMustGoOne()
 	--а за тем избавимся от пустых фрагментов (0x2800)
 	for y in pairs(chars)do
 		for x in pairs(chars[y])do
-            a=unicode.char(chars[y][x])
+            a = unicode.char(chars[y][x])
             gpu.set(x,y,a)
 		end
-	end
-  return 'обновления успешно отображены'
+	end  return iteration()
 end
 ---для перехода к следующему витку поменяем ссылки на левое и правое
 function swap()
     l,r = r,l
-    return 'swap complete'
+    return true
 end
 
 --здесь мы проверяем не надоело ли пользователю лицезреть эволюцию
 function allOK()
     local status = true 
-    --some comparisions
+    if mode == 'edit' then status = false end
     return status
 end
-
+function iteration()
+    text = tostring(iter)..' '..tostring(dots)..' '
+    gpu.set(8,y_dim,text)
+    if dots == 0 then return true end
+    iter = iter+1
+    os.sleep(0.05)
+    return swap()
+end
 --вроде всё готово к началу работы програмки
 tablesInit()
 --запрашиваем начальное состояние поля
-userInput(l)
 --записываем полученные данные в мониторинг посредством screen
-saveChanges()
 ---создадим цикл, чтобы всё работало
-toUnicode()
-showMustGoOne()
 
+function main()
 while allOK() do 
     whatNews(l,r)
-    toUnicode()
-    showMustGoOne()
-    swap()
-    text = tostring(iter)..' '..tostring(dots)..' '
-    gpu.set(1,y_dim,text)
-    iter=iter+1
-    os.sleep(0.1)
+end
 end
 
+main()
 --В итоге у нас получилась довольно симпатичная програмка
 --Возможно, в будущем я найду интерес и время
 --чтобы дописать вывод на экран и реализовать интерактивность
